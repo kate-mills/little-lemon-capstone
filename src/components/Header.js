6636fb2e4c1b530a703550df1002cd802d09940c
Kate Mills <helloworldloveksm@icloud.com>
2023-01-20 15:41:27 -0800

    mv Nav element to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 
+import Nav from './Nav'
 import logo from '../assets/logo.svg'
 
 const Header = () => {
   return (
     <header>
       <img src={logo} alt="Little Lemon Logo" />
+      <Nav />
     </header>
   )
 }
