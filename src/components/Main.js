06ee22a303c358dbe757540a18804107a52f4ad8
Kate Mills <helloworldloveksm@icloud.com>
2023-01-22 18:04:08 -0800

    Basic figma alignment

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,14 @@
 import * as React from 'react'
+import heroimg from '../assets/hero-title.svg'
 const Main = () => {
   return (
+
     <main>
-      <article>Hero Article</article>
+      <article>
+    <img src={heroimg} width="302" alt=""/>
+      <button className="btn-hero">Reserve a Table</button>
+
+    </article>
       <article>Specials Article</article>
       <article>Testimonials Article</article>
       <article>About Article</article>
