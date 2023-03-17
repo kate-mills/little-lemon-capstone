8a26210f4021f8c43eb6793d7e82427794bab12f
Kate Mills <helloworldloveksm@icloud.com>
2023-03-17 13:08:44 -0700

    testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,9 +3,20 @@ import styles from '../assets/css/testimonials.module.css'
 
 const Testimonials = () => {
   return (
-    <article id={styles.testimonials}>
-    <h1>Testimonials</h1>
-    </article>
+    <>
+      <article id={styles.testimonials}>
+        <h1>Testimonials</h1>
+      </article>
+
+        <div className={styles.gridAdjustableColumns}>
+          <div>g1</div>
+          <div>g1</div>
+          <div>g1</div>
+          <div>g1</div>
+          <div>g1</div>
+          <div>g1</div>
+        </div>
+    </>
   )
 }
 export default Testimonials
