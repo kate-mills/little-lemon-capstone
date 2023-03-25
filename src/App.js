33ccbf45951a6341aafdd124b6ffcd03165751d8
Kate Mills <helloworldloveksm@icloud.com>
2023-03-25 12:05:46 -0700

    footer outside main tag

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <>
-      <div className="block-bg" style={{background: 'var(--white)'}}>
+      <div className="block-bg" style={{ background: 'var(--white)' }}>
         <div className="block">
           <Header />
         </div>
       </div>
+
       <main>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/reservations" element={<ReservationsPage />}></Route>
         </Routes>
-
-      <div className="block-bg" style={{background: 'var(--gray)'}}>
-        <div className="block">
-        <Footer />
-    </div></div>
       </main>
+
+      <div className="block-bg" style={{ background: 'var(--gray)' }}>
+        <div className="block">
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
