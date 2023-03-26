2c8b37a7c7dd758185a55699f78391506408d147
Kate Mills <helloworldloveksm@icloud.com>
2023-03-26 16:10:52 -0700

    style attr first

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -2,7 +2,7 @@ import styles from './index.module.css'
 
 const Block = ({ bgColor = 'var(--gray)', style={}, children }) => {
   return (
-    <div className={styles.block_bg} style={{ background: bgColor, ...style }}>
+    <div  style={{ background: bgColor, ...style }} className={styles.block_bg}>
       <div className={styles.block}>{children}</div>
     </div>
   )
