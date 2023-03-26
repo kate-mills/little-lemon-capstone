73f513d377d499f6d8b6f99b71099eb6a7f78897
Kate Mills <helloworldloveksm@icloud.com>
2023-03-25 17:07:48 -0700

    About Section

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,25 @@
-import * as React from 'react'
 import styles from '../assets/css/about.module.css'
+import HeroTitle from './HeroTitle'
+
+import BlockBg from './BlockBg'
+import brothers from '../assets/brothers.png'
 
 const About = () => {
   return (
-    <article id={styles.about}>
-    <h1>About</h1>
-    </article>
+    <BlockBg bgColor="#36433E">
+      <article id={styles.about}>
+        <div className={styles.text_box}>
+          <HeroTitle />
+          <p className={styles.paragraph_text}>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+        </div>
+        <div className={styles.image_box}>
+          <img src={brothers} alt="Photos of Adrian & Mario" />
+        </div>
+      </article>
+    </BlockBg>
   )
 }
-export default About 
+export default About
