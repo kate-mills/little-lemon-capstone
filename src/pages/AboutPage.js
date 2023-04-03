82eebc259920e91f6bd04b8d8bb803819df93c25
Kate Mills <helloworldloveksm@icloud.com>
2023-04-02 17:29:40 -0700

    AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,12 +1,15 @@
 import Testimonials from '../components/Testimonials'
+import Specials from '../components/WeeklySpecials'
 import About from '../components/About'
 
+import menu_items from '../constants/weekly-specials'
 
 const AboutPage = () => {
   return (
     <>
-    <About/>
-    <Testimonials/>
+      <About/>
+      <Specials menu_items={menu_items}/>
+      <Testimonials/>
     </>
   )
 }
