06c1cbec48e3ee31d7c2f059b385a0e009180282
Kate Mills <helloworldloveksm@icloud.com>
2023-04-04 16:21:03 -0700

    mv ReserveATablePage BookingPage

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,15 +1,15 @@
 import Block from '../components/Block'
 import FormReserveATable from '../components/FormReserveATable'
 
-const ReserveATablePage = () => {
+const BookingPage = () => {
   return (
     <>
     <Block bgColor="var(--clr-highlight)">
-      <h1>Reserve A Table</h1>
+      <h1>Book A Table</h1>
     </Block>
     <FormReserveATable/>
     </>
   )
 }
 
-export default ReserveATablePage
+export default BookingPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import HomePage from './HomePage'
 import AboutPage from './AboutPage'
 import MenuPage from './MenuPage'
 import ReserveATablePage from './ReserveATablePage'
+import BookingPage from './BookingPage'
 import OrderOnlinePage from './OrderOnlinePage'
 import LoginPage from './LoginPage'
 import ErrorPage from './ErrorPage'
@@ -11,6 +12,7 @@ export {
   AboutPage,
   MenuPage,
   ReserveATablePage,
+  BookingPage,
   OrderOnlinePage,
   LoginPage,
   ErrorPage
