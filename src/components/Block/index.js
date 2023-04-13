f0470d20580ce174879b0a15085662295ca9822f
Kate Mills <helloworldloveksm@icloud.com>
2023-04-13 16:39:08 -0700

    semantic element

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -2,9 +2,9 @@ import styles from './index.module.css'
 
 const Block = ({ bgColor = 'var(--gray)', style = {}, children }) => {
   return (
-    <div style={{ background: bgColor, ...style }} className={styles.block_bg}>
+    <section style={{ background: bgColor, ...style }} className={styles.block_bg}>
       <div className={styles.block}>{children}</div>
-    </div>
+    </section>
   )
 }
 
