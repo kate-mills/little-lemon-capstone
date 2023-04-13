524199a891b647f60e3f7456302d4cdb2ed99ba2
Kate Mills <helloworldloveksm@icloud.com>
2023-04-13 11:35:01 -0700

    Bookings client-side validation tests passing

--- src/Bookings.test.js
+++ src/Bookings.test.js
@@ -1,54 +1,60 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 
 import { BookingProvider } from './context/book-a-table'
-import {BookingPage, ConfirmedBookingPage} from './pages/index.js'
-import BookingsForm from "./components/Bookings/Form.js";
-import BookingsConfirmed from "./components/Bookings/Confirmed.js";
-import {endDate} from './utils/get-reservation-dates.js'
-import {fetchAPI} from './api'
-
-
+import { BookingPage, ConfirmedBookingPage } from './pages/index.js'
+import BookingsForm from './components/Bookings/Form.js'
+import BookingsConfirmed from './components/Bookings/Confirmed.js'
+import { endDate } from './utils/get-reservation-dates.js'
+import { fetchAPI } from './api'
 
 test('Static text book a table being rendered in the Bookings component', () => {
   render(
     <BookingProvider>
       <BookingPage />
     </BookingProvider>
-  );
+  )
 
-  const titleElement = screen.getByText(/book a table/i);
-  expect(titleElement).toBeDefined();
-});
-
+  const titleElement = screen.getByText(/book a table/i)
+  expect(titleElement).toBeDefined()
+})
 
 test('BookingsForm component can be submitted by the user', async () => {
-
   const [time] = fetchAPI(new Date(endDate))
-  const handleSubmit = jest.fn();
+
+  const handleSubmit = await jest.fn()
 
   render(
     <BookingProvider>
       <BookingsForm onSubmit={handleSubmit} />
-      <ConfirmedBookingPage/>
+      <ConfirmedBookingPage />
     </BookingProvider>
-  );
-  const submitButton = screen.getByRole("button")
+  )
+  const submitButton = screen.getByRole('button')
 
   const dateInput = screen.getByLabelText(/Select Date/i)
   const timeInput = screen.getByLabelText(/Select Time/i)
   const guestInput = screen.getByLabelText(/Number Of Guests/i)
+  const occasionInput = screen.getByLabelText(/Occasion/i)
 
-  expect(submitButton).toHaveAttribute("disabled")
 
-  fireEvent.change(dateInput, {target: {value: endDate}})
-  fireEvent.change(timeInput, {target: {value: time}})
-  fireEvent.change(guestInput, {target: {value: 3}})
+  expect(submitButton).toHaveAttribute('disabled')
 
-  expect(submitButton).not.toHaveAttribute("disabled")
+  await act(async () => {
+    fireEvent.change(dateInput, { target: { value: endDate } })
+    fireEvent.change(timeInput, { target: { value: time } })
+    fireEvent.change(guestInput, { target: { value: 3 } })
+    fireEvent.change(occasionInput, { target: { value: "No Occasion" } })
+  })
+
+  expect(submitButton).not.toHaveAttribute('disabled')
   expect(handleSubmit).not.toHaveBeenCalled()
-  fireEvent.submit(submitButton)
+
+  await act(async () => {
+    fireEvent.submit(submitButton)
+  })
 
   // redirects to confirmed booking page
-  const confirmedHeading = screen.getByText(/Your Booked on/i);
-  expect(confirmedHeading).toBeDefined();
-});
+  const confirmedHeading = screen.getByText(/Your Booked on/i)
+  expect(confirmedHeading).toBeDefined()
+})
